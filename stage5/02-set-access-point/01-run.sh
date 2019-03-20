194f85df120eb62ace7303d919a991fe672e7210
sailoog <info@sailoog.com>
2019-03-20 20:48:34 +0100

    new network management

--- stage5/02-set-access-point/01-run.sh
+++ stage5/02-set-access-point/01-run.sh
@@ -28,6 +28,8 @@ install -m 644 files/interfaces	"${ROOTFS_DIR}/etc/network/"
 install -m 644 files/ap		"${ROOTFS_DIR}/etc/network/interfaces.d/"
 install -m 644 files/station		"${ROOTFS_DIR}/etc/network/interfaces.d/"
 install -m 644 files/72-static-name.rules		"${ROOTFS_DIR}/etc/udev/rules.d/"
+install -m 644 files/11-openplotter-usb0.rules		"${ROOTFS_DIR}/etc/udev/rules.d/"
+install -m 644 files/add_hostname_dot_local.sh		"${ROOTFS_DIR}/home/pi/"
 
 if [ -e "${ROOTFS_DIR}/etc/udev/rules.d/90-wireless.rules" ]
 then
@@ -38,3 +40,23 @@ if [ -e "${ROOTFS_DIR}/lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant" ]
 then
     mv "${ROOTFS_DIR}/lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant" "${ROOTFS_DIR}/lib/dhcpcd"
 fi
+
+on_chroot << EOF
+erg=$(systemctl status dnsmasq | grep disabled)
+chrlen=${#erg}
+if [ $chrlen -gt 0 ]
+then
+	systemctl enable dnsmasq
+fi
+
+erg=$(systemctl status hostapd | grep disabled)
+chrlen=${#erg}
+if [ $chrlen -gt 0 ]
+then
+	systemctl enable hostapd
+fi
+
+cd
+/bin/bash add_hostname_dot_local.sh
+rm -f add_hostname_dot_local.sh
+EOF
