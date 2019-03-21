f83577a820236ea36b415e5b8acbd022abf8056b
sailoog <info@sailoog.com>
2019-03-21 22:02:11 +0100

    add bookmarks

--- stage5/07-install-openplotter/01-run.sh
+++ stage5/07-install-openplotter/01-run.sh
@@ -47,6 +47,7 @@ install -m 644 -o 1000 -g 1000 files/signalk.desktop		"${ROOTFS_DIR}/home/pi/.lo
 install -m 644 -o 1000 -g 1000 files/signalk.ico		"${ROOTFS_DIR}/home/pi/.local/share/applications/"
 install -m 644 -o 1000 -g 1000 files/desktop-items-0.conf		"${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE-pi/"
 install -m 644 -o 1000 -g 1000 files/pcmanfm.conf		"${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE-pi/"
+install -m 644 -o 1000 -g 1000 files/.gtk-bookmarks		"${ROOTFS_DIR}/home/pi/"
 install -m 644 files/xygrib.desktop		"${ROOTFS_DIR}/usr/share/applications/"
 install -m 644 files/opencpn.desktop		"${ROOTFS_DIR}/usr/share/applications/"
 install -m 644 files/lxde-pi-applications.menu		"${ROOTFS_DIR}/etc/xdg/menus/"
